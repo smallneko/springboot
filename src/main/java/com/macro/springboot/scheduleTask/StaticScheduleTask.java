8ca53466dfa5d7494eaeb81e33de15cdd9563cc8
macro <smallneko11@163.com>
2019-04-19 09:49:25 +0800

    Logger依赖和方法添加

--- src/main/java/com/macro/springboot/scheduleTask/StaticScheduleTask.java
+++ src/main/java/com/macro/springboot/scheduleTask/StaticScheduleTask.java
@@ -1,5 +1,7 @@
 package com.macro.springboot.scheduleTask;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -12,12 +14,16 @@ import java.time.LocalDateTime;
 @EnableScheduling
 public class StaticScheduleTask {
 
+	//Logger方法添加
+	final Logger logger = LoggerFactory.getLogger(StaticScheduleTask.class);
+
 	//添加定时任务
 	@Scheduled(cron = "0/5 * * * * ?")
 	//或直接指定时间间隔，例如：5秒
 	//@Scheduled(fixedRate=5000)
 	private void configureTasks() {
 		System.err.println("执行静态定时任务时间: " + LocalDateTime.now());
+		logger.info("Logger方法测试");
 	}
 
 }
